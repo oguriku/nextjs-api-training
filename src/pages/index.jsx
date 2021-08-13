9c7327f394777605dccd571a3795c9e86c7ed213
riku <you@example.com>
2021-08-13 18:47:59 +0900

    ✨

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { useCallback, useState } from 'react'
 
 import { Footer } from '../components/Footer'
 import { Header } from '../components/Header'
@@ -7,6 +8,20 @@ import { Main } from '../components/Main'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const [text,setText] = useState("");
+  const [array, setArray] = useState([]);
+
+  const handleAdd = useCallback(() => {
+    setArray((array) => {
+      if(array.some(item => item === text)) {
+        alert("同じ要素が既に存在します")
+        return array;
+      }  
+      return [...array, text]
+    }
+    )
+  },[text])
+
   return (
     <div className={styles.container}>
       <Head>
@@ -15,6 +30,17 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header/>
+
+      <input type="text" value={text} onChange={(e) => setText(e.target.value)}/>
+      <button onClick={handleAdd}>追加</button>
+      <ul>
+        {array.map(item => {
+          return (
+            <li key={item}>{item}</li>
+          )
+        })}
+      </ul>
+
       <Main page="index" />
       <Footer/>
 
