590f8dbe9dbd49797a1efd90efbecc2f6665ea4a
riku <you@example.com>
2021-08-18 16:25:14 +0900

    ✨ コンポーネント間のリフトアップ

--- src/components/Headline.jsx
+++ src/components/Headline.jsx
@@ -1,16 +1,18 @@
 
 import styles from 'src/components/Headline.module.css'
 
-export　function Headline(props) {
+export function Headline(props) {
     return (
         <div>
             <h1 className={styles.title}>
-            {props.page} page
+                {props.page} page
             </h1>
 
             <p className={styles.description}>
-            Get started by editing  {props.children}
+                アイテムの数は  {props.children}　個です。
             </p>
+
+            <button onClick={props.handleReduce}>減らす</button>
         </div>
     )
 }
