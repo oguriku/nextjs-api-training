e48d61d587168e7df0478c865fcc2d339f1ab06f
riku <you@example.com>
2021-08-21 00:13:43 +0900

    ✨ APIを叩いた

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,22 @@
 import Head from 'next/head'
+import { useCallback, useEffect, useState } from 'react'
 
-import { Footer } from '../components/Footer'
 import { Header } from '../components/Header'
-import { Main } from '../components/Main'
 import styles from '../styles/Home.module.css'
 
 export default function Home(props) {
-  const { text, array, handleAdd, onChangeValue } = props;
+
+  const [posts, setPosts] = useState([]);
+
+  const getPosts = useCallback(async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const json = await res.json();
+    setPosts(json);
+  }, [])
+
+  useEffect(() => {
+    getPosts();
+  }, [getPosts])
 
   return (
     <div className={styles.container}>
@@ -17,18 +27,13 @@ export default function Home(props) {
       </Head>
       <Header />
 
-      <input type="text" value={text} onChange={onChangeValue} />
-      <button onClick={handleAdd}>追加</button>
-      <ul>
-        {array.map(item => {
+      <ol>
+        {posts.map(post => {
           return (
-            <li key={item}>{item}</li>
+            <li key={post.id}>{post.title}</li>
           )
         })}
-      </ul>
-
-      <Main page="index" />
-      <Footer />
+      </ol>
     </div>
   )
 }
