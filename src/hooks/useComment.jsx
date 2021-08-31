250f9aef07af8d27c54e9fea22b6b126a8f96b19
riku <you@example.com>
2021-08-31 11:07:50 +0900

    ♻️ Comment

---
+++ src/hooks/useComment.jsx
@@ -0,0 +1,17 @@
+
+import useSWR from "swr"
+import { useRouter } from "next/dist/client/router";
+
+import { fetcher } from "src/utils/fetcher"
+
+export const useComment = () => {
+    const router = useRouter();
+    const { data, error } = useSWR(router.query.id ? `https://jsonplaceholder.typicode.com/comments/${router.query.id}`: null, fetcher);
+
+    return {
+        data,
+        error,
+        isLoading: !error && !data,
+        isEmpty: data && data.length === 0,
+    };
+}
--- src/pages/comments/[id]/index.jsx
+++ src/pages/comments/[id]/index.jsx
@@ -1,42 +1,8 @@
-import { useRouter } from "next/dist/client/router";
+
 import Head from "next/head";
+
+import { CommentComponent } from "src/components/Comment/CommentComponent";
 import { Header } from "src/components/header/Header";
-import { fetcher } from "src/utils/fetcher"
-import useSWR from "swr"
-
-const useComment = () => {
-    const router = useRouter();
-    const { data, error } = useSWR(router.query.id ? `https://jsonplaceholder.typicode.com/comments/${router.query.id}`: null, fetcher);
-
-    return {
-        data,
-        error,
-        isLoading: !error && !data,
-        isEmpty: data && data.length === 0,
-    };
-}
-
-const CommentComponent = () => {
-
-    const { data, error, isLoading } = useComment();
-
-    if (isLoading) {
-        return (<p>loding now ....</p>);
-    }
-    if (error) {
-        return (<div>{error.message}</div>);
-    }
-
-    return (
-        <div>
-            <h1>{data.body}</h1>
-            <ul>
-                <li>{data.name}</li>
-                <li>{data.email}</li>
-            </ul>
-        </div>
-    )
-}
 
 const Comment = () => {
 
