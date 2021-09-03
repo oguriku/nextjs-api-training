57dbe30c4febe45deac1ed78e1b012cb1d43b42f
riku <you@example.com>
2021-09-04 00:03:45 +0900

    ♻️

--- src/hooks/useFetchArray.jsx
+++ src/hooks/useFetchArray.jsx
@@ -14,22 +14,24 @@ const useFetchArray = (url) => {
     };
 };
 
+//posts
 export const usePosts = () => {
     return useFetchArray("https://jsonplaceholder.typicode.com/posts");
 };
 
+export const usePostsByUserId = id => {
+    return useFetchArray(id ? `https://jsonplaceholder.typicode.com/posts?userId=${id}` : null)
+}
+// users
 export const useUsers = () => {
     return useFetchArray("https://jsonplaceholder.typicode.com/users");
 };
 
+// comments
 export const useComments = () => {
     return useFetchArray("https://jsonplaceholder.typicode.com/comments");
 };
 
 export const useCommentsByPostId = id => {
     return useFetchArray(id ? `https://jsonplaceholder.typicode.com/comments?postId=${id}` : null)
-}
-
-export const usePostsByUserId = id => {
-    return useFetchArray(id ? `https://jsonplaceholder.typicode.com/posts?userId=${id}` : null)
 }
