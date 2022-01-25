e277431db8ccfdff9c8308a486c93fa8eb430127
riku <you@example.com>
2022-01-25 21:20:49 +0900

    ♻️

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -14,7 +14,7 @@ export function Header() {
     <header className={styles.header}>
       {navItems.map(item => {
         return (
-          <Link href={item.href}>
+          <Link href={item.href} key={item.href}>
             <a className={styles.anchor}>{item.label}</a>
           </Link>
         )
