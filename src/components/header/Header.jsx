0302417794c8930fbeb7cf22468de77440d4169b
riku <you@example.com>
2022-01-25 21:30:33 +0900

    ♻️

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link"
 
-import styles from 'src/components/header/Header.module.css'
+import styles from 'src/components/Header/Header.module.css'
 
 export function Header() {
   const navItems = [
